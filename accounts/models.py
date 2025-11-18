08202c553f0c735e583f0f65fa30c82daef8395e
Brendan Brown <brendanriesbrown@gmail.com>
2025-11-18 14:21:24 -0500

    Exam 3 User Story

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+def user_profile_image_path(instance, filename):
+	# Files will be uploaded to MEDIA_ROOT/profile_pics/user_<id>/<filename>
+	return f'profile_pics/user_{instance.user.id}/{filename}'
+
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to=user_profile_image_path, blank=True, null=True)
+
+	def __str__(self):
+		return f"{self.user.username} Profile"
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+	else:
+		# Ensure profile exists and save
+		try:
+			instance.profile.save()
+		except Exception:
+			Profile.objects.get_or_create(user=instance)
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
-from .forms import CustomUserCreationForm, CustomErrorList
+from .forms import CustomUserCreationForm, CustomErrorList, ProfileForm
+from .models import Profile
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -48,3 +49,24 @@ def orders(request):
     template_data['orders'] = request.user.order_set.all()
     return render(request, 'accounts/orders.html',
         {'template_data': template_data})
+
+
+@login_required
+def profile(request):
+    template_data = {}
+    template_data['title'] = 'Profile'
+    # Ensure profile exists for the user
+    profile_obj, _ = Profile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts.profile')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/profile.html', {'template_data': template_data})
+    else:
+        form = ProfileForm(instance=profile_obj)
+        template_data['form'] = form
+        return render(request, 'accounts/profile.html', {'template_data': template_data})
